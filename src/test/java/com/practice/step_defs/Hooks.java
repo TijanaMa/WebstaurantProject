a225b15c62b56e061d09adb88fd72266e4524e95
tijana <tijana822@gmail.com>
2023-06-14 23:50:05 -0400

    hooks class

--- src/test/java/com/practice/step_defs/Hooks.java
+++ src/test/java/com/practice/step_defs/Hooks.java
@@ -1,7 +1,48 @@
 package com.practice.step_defs;
 
+import com.practice.utilities.DB_Util;
+import com.practice.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
 
+
+    @Before("@db")
+    public void dbHook() {
+        System.out.println("creating database connection");
+        DB_Util.createConnection();
+    }
+
+    @After("@db")
+    public void afterDbHook() {
+        System.out.println("closing database connection");
+        DB_Util.destroy();
+
+    }
+
+    @Before("@ui")
+    public void setUp() {
+        // we put a logic that should apply to every scenario
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @After("@ui")
+    public void tearDown(Scenario scenario) {
+        // only takes a screenshot if the scenario fails
+        if (scenario.isFailed()) {
+            // taking a screenshot
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png","screenshot");
+        }
+        Driver.closeDriver();
+    }
 
 
 
